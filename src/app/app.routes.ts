c30894487b322e2a2a2e5690c739dafddc3e87db
Rubens Marcondes <rubenspotcake@proton.me>
2025-09-30 21:23:26 -0300

    Primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ConsultaComponent } from './components/consulta/consulta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'cadastro', component: CadastroComponent},
+  {path:'consulta', component: ConsultaComponent},
+];
